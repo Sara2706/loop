4d414f539b32882b8a89db3182c9b3b1b98e6e43
Sara2706 <101634428+Sara2706@users.noreply.github.com>
2022-07-29 01:30:54 -0700

    Add files via upload

---
+++ loops/script.js
@@ -0,0 +1,14 @@
+// start loops
+
+// Define row here
+for(let i = 1 ; i <= 5 ; i++){
+    let num = 0;
+    // Define coloumn here
+    for(let j = 1 ; j <= i ; j++){
+        // Define output here
+        num = j + " ";
+        document.write(num);
+    };
+    // Output
+    document.write('<br>');
+}
